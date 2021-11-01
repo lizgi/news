9c8fcb24a6ac8ef0f76512a1f8d15a8790148b97
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-01 13:38:06 +0300

    get api key

--- app/requests.py
+++ app/requests.py
@@ -5,11 +5,3 @@ class NewsRequests:
     API_KEY = '3ca9064b0bd54301864b51412d590ede'
     n = NewsApiClient(api_key=API_KEY)
     
-    def get_top_headlines(self,**kwargs):
-        return self.n.get_top_headlines(**kwargs)
-    
-    def get_everything(self,**kwargs):
-        return self.n.get_everything(**kwargs)
-    
-    def get_sources(self,**kwargs):
-        return self.n.get_sources(**kwargs)
