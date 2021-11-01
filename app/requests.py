43377f9333436c7ad3786a63deeb3e1d4f552309
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-01 13:05:36 +0300

    installations

--- app/requests.py
+++ app/requests.py
@@ -2,7 +2,7 @@ from newsapi import NewsApiClient
 # from ..config import from news
 
 class NewsRequests:
-    API_KEY = '1c6caded5f054d598bf505ff7f0491a6'
+    API_KEY = '3ca9064b0bd54301864b51412d590ede'
     n = NewsApiClient(api_key=API_KEY)
     
     def get_top_headlines(self,**kwargs):
